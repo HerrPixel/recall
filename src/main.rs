44b40c52ad507bb9f10e1a63f5f62953c5038ec4
Jonas Seiler <Me@jonasseiler.de>
2025-01-20 21:57:10 +0100

    add ratatui skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{DefaultTerminal, Frame};
+use std::io;
+
+#[derive(Debug, Default)]
+struct AppConfig {
+    highlight_color: ratatui::style::Color,
+}
+
+#[derive(Debug, Default)]
+struct App {
+    config: AppConfig,
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        todo!()
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+
+    app_result
 }
