cc8645bc5530e4b9767de019143e0412e66d73ec
Jonas Seiler <Me@jonasseiler.de>
2025-01-21 18:39:06 +0100

    refine ui skeleton

--- src/ui.rs
+++ src/ui.rs
@@ -1,27 +1,42 @@
 use ratatui::{
-    layout::Rect,
-    style::{Color, Style},
-    text::Text,
-    widgets::{Block, Borders, Paragraph},
+    layout::{Constraint, Direction, Layout},
+    text::{Line, Span},
+    widgets::{Block, Paragraph},
     Frame,
 };
 
 use crate::app::App;
 
 pub fn ui(frame: &mut Frame, app: &App) {
-    let title_block = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default());
+    if !app.has_config {
+        render_no_config_hint(frame);
+        return;
+    }
 
-    let title_string = match app.number_of_pages {
-        0 => "placeholder",
-        _ => &app.pages[app.current_page].title,
-    };
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Length(1),
+            Constraint::Min(1),
+            Constraint::Length(1),
+        ])
+        .split(frame.area());
 
-    let title = Paragraph::new(Text::styled(title_string, Style::default().fg(Color::Blue)))
-        .block(title_block);
+    let line = Line::from(vec![Span::raw("="), Span::raw("Titlelol"), Span::raw("=")])
+        .alignment(ratatui::layout::Alignment::Center);
 
-    let area = Rect::new(0, 0, 10, 10);
+    frame.render_widget(line, layout[0]);
+}
 
-    frame.render_widget(title, area);
+pub fn render_no_config_hint(frame: &mut Frame) {
+    let block = Block::bordered();
+
+    let paragraph = Paragraph::new(vec![
+        "It seems like there is no config loaded!".into(),
+        "To Load a config, use <insert> or <insert>".into(),
+    ])
+    .centered()
+    .block(block);
+
+    frame.render_widget(paragraph, frame.area());
 }
