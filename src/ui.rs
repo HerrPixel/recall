585a252513053472f209d8c5df07186256782288
Jonas Seiler <Me@jonasseiler.de>
2025-01-22 10:43:27 +0100

    add more boilerplate code to ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,7 @@
 use ratatui::{
     layout::{Constraint, Direction, Layout},
-    text::{Line, Span},
+    style::Style,
+    text::{Line, Span, Text},
     widgets::{Block, Paragraph},
     Frame,
 };
@@ -13,6 +14,8 @@ pub fn ui(frame: &mut Frame, app: &App) {
         return;
     }
 
+    let curr_page = &app.sections[app.current_section];
+
     let layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints(vec![
@@ -22,10 +25,27 @@ pub fn ui(frame: &mut Frame, app: &App) {
         ])
         .split(frame.area());
 
-    let line = Line::from(vec![Span::raw("="), Span::raw("Titlelol"), Span::raw("=")])
-        .alignment(ratatui::layout::Alignment::Center);
+    let line = Line::from(vec![
+        Span::raw("="),
+        Span::raw(curr_page.title.clone()),
+        Span::raw("="),
+    ])
+    .alignment(ratatui::layout::Alignment::Center);
 
     frame.render_widget(line, layout[0]);
+
+    let mut list = vec![];
+
+    for item in &curr_page.items {
+        list.push(Line::from(vec![
+            Span::styled(item.0.clone(), Style::default().fg(app.highlight_color)),
+            Span::styled(item.1.clone(), Style::default().fg(app.primary_color)),
+        ]));
+    }
+
+    let p = Paragraph::new(Text::from(list));
+
+    frame.render_widget(p, layout[1]);
 }
 
 pub fn render_no_config_hint(frame: &mut Frame) {
