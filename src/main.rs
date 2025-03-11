68b888f5e19c6fc1ed6aecd6bdf278f6d8123a05
Jonas Seiler <Me@jonasseiler.de>
2025-03-11 10:19:27 +0100

    use anyhow native result

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use anyhow::{Error, Ok};
+use anyhow::Result;
 use clap::Parser;
 use cli::Commands;
 use ratatui::{
@@ -19,7 +19,11 @@ use cli::Cli;
 use config::{default_config_path, init_config, read_from_config};
 use ui::ui;
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() -> Result<()> {
+    // TODO: Return correct exit codes
+
+    // use color_eyre
+
     let cli = Cli::parse();
 
     let config_path = cli.config.unwrap_or(default_config_path()?);
@@ -35,14 +39,13 @@ fn main() -> Result<(), anyhow::Error> {
     app.add_config(config);
 
     let mut terminal = ratatui::init();
-
     run(&mut terminal, &mut app)?;
     ratatui::restore();
 
     Ok(())
 }
 
-fn run<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> Result<(), Error> {
+fn run<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> Result<()> {
     while app.active {
         terminal.draw(|f| ui(f, app))?;
 
@@ -73,7 +76,7 @@ fn handle_subcommands(
     command: Option<Commands>,
     app: &mut App,
     config_path: PathBuf,
-) -> Result<(), Error> {
+) -> Result<()> {
     if let Some(Commands::Init) = command {
         // Remove this in favor of logging
         println!("{}", init_config(config_path.clone())?);
