1f6f6531c08d87f8121036563a2219668dc23d81
Jonas Seiler <Me@jonasseiler.de>
2025-03-11 11:12:45 +0100

    add logging to main module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use anyhow::Result;
 use clap::Parser;
 use cli::Commands;
+use log::{info, trace};
 use ratatui::{
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     prelude::Backend,
@@ -19,29 +20,57 @@ use cli::Cli;
 use config::{default_config_path, init_config, read_from_config};
 use ui::ui;
 
+// TODO: Function comments
+
 fn main() -> Result<()> {
     // TODO: Return correct exit codes
 
-    // use color_eyre
+    // TODO: Use color_eyre
 
+    trace!("Parsing CLI arguments");
     let cli = Cli::parse();
 
-    let config_path = cli.config.unwrap_or(default_config_path()?);
+    // TODO: What if path contains illegal unicode symbols?
+    //       -> Dangerous unwrap
+    let config_path = match cli.config {
+        Some(path) => {
+            info!("Using custom config path: {}", path.to_str().unwrap());
+            path
+        }
+        None => {
+            let path = default_config_path()?;
+            info!("Using default config path: {}", path.to_str().unwrap());
+            path
+        }
+    };
+
     let mut app = App::new();
 
+    trace!("Parsing CLI subcommands");
     handle_subcommands(cli.command, &mut app, config_path.clone())?;
 
     if !app.active {
+        info!("Quitting due to completed subcommand");
         return Ok(());
     }
+
+    info!("Reading config from {}", config_path.to_str().unwrap());
     // TODO: Handle non-existent config without throwing an error
     let config = read_from_config(config_path)?;
+
+    // This seems like bad style, maybe it's best to temporarily decouple
+    // the state from the app and make one constructor that takes the config and state
+    // without having a partially initialized struct like here.
     app.add_config(config);
 
+    trace!("Creating terminal backend");
     let mut terminal = ratatui::init();
-    run(&mut terminal, &mut app)?;
-    ratatui::restore();
 
+    trace!("Starting main loop");
+    run(&mut terminal, &mut app)?;
+
+    trace!("Restoring terminal");
+    ratatui::restore();
     Ok(())
 }
 
@@ -49,7 +78,9 @@ fn run<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> Result<()> {
     while app.active {
         terminal.draw(|f| ui(f, app))?;
 
+        // TODO: Non-blocking with poll
         if let Event::Key(key) = event::read()? {
+            trace!("Handling key event");
             handle_key_event(key, app)
         }
     }
@@ -58,16 +89,32 @@ fn run<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> Result<()> {
 }
 
 fn handle_key_event(key: KeyEvent, app: &mut App) {
+    // TODO: Check that the keys are pressed, not repeated or released
+
+    // Is this the correct way to handle SIGINTs and SIGKILLs?
     if key.modifiers == KeyModifiers::CONTROL {
         if let KeyCode::Char('c') = key.code {
+            info!("Quitting due to received SIGINT Signal");
+            // TODO: Might want to add reasond for quitting
             app.active = false;
         }
     } else {
         match key.code {
-            KeyCode::Left => app.decrement_page(),
-            KeyCode::Right => app.increment_page(),
-            KeyCode::Char('q') => app.active = true,
-            _ => {}
+            KeyCode::Left => {
+                trace!("Decremting page number");
+                app.decrement_page()
+            }
+            KeyCode::Right => {
+                trace!("Incrementing page number");
+                app.increment_page()
+            }
+            KeyCode::Char('q') => {
+                info!("Quitting due to pressed 'quit' button");
+                app.active = false
+            }
+            _ => {
+                trace!("Unused key(s) pressed: {}+{}", key.modifiers, key.code);
+            }
         }
     }
 }
@@ -77,9 +124,19 @@ fn handle_subcommands(
     app: &mut App,
     config_path: PathBuf,
 ) -> Result<()> {
+    // TODO: When more subcommands are added, do `match` instead of `if let`
+
     if let Some(Commands::Init) = command {
-        // Remove this in favor of logging
-        println!("{}", init_config(config_path.clone())?);
+        // TODO: What if path contains illegal unicode symbols?
+        //       -> Dangerous unwrap
+        info!(
+            "Creating initial config in {}",
+            config_path.to_str().unwrap()
+        );
+
+        let _ = init_config(config_path)?;
+
+        // TODO: Use a state enum instead of a boolean. Is more semantically meaningful.
         app.active = false;
     }
     Ok(())
