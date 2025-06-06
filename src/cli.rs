ebe305523dae6de1b91ffaeec52b9f5a28d2e3db
Jonas Seiler <Me@jonasseiler.de>
2025-06-06 09:13:43 +0200

    Added more documentation for CLI module

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,25 @@
-use std::path::PathBuf;
+//! CLI Definition for Recall.
+//!
+//! This module defines the command-line interface using the [`clap`] crate.
 
 use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
+/// Clap CLI Configuration
 #[derive(Parser)]
 #[command(about = "Recall Keybinds, shortcuts, commands and more",long_about=None)]
 #[command(version)]
 pub struct Cli {
-    /// Specify a different configuration file
+    /// Path to a different configuration file
     #[arg(short, long, value_name = "FILE")]
     pub config: Option<PathBuf>,
 
+    /// Subcommands for the CLI
     #[command(subcommand)]
     pub command: Option<Commands>,
 }
 
+/// Available subcommands
 #[derive(Subcommand)]
 pub enum Commands {
     /// Initialize example config
